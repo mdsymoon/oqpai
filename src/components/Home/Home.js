2ec11d0018fe09b1cd251eafba82313814f62479
mdsymoon <mdsymoon11@gmail.com>
2021-11-20 11:45:43 -0800

    projest hosting

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import Navigation from '../Navigation/Navigation';
-import TopSection from '../TopSection/TopSection';
+import ServiceSection from '../ServiceSection/ServiceSection';
 
 
 const Home = () => {
     return (
         <div>
             <Navigation/>
-            <TopSection/>
+            <ServiceSection/>
         </div>
     );
 };
--- src/components/ServiceSection/ServiceSection.js
+++ src/components/ServiceSection/ServiceSection.js
@@ -2,12 +2,15 @@ import React from "react";
 import topSectionImg from "../../images/topsectionImg.jpg";
 import bottomSectionImg from "../../images/buttomSectionImg.jpg";
 
-const TopSection = () => {
+const ServiceSection = () => {
   return (
     <main className="TopSection mt-20">
-      <div className="flex justify-center flex-col md:flex-row">
+      {/* top Section */}
+      <div className="flex flex-col md:flex-row">
         <div className="flex-1">
-          <h1 className="sectionTitle ml-10 md:ml-20">Worrying about luggage?</h1>
+          <h1 className="sectionTitle ml-10 md:ml-20">
+            Worrying about luggage?
+          </h1>
           <p className="sectionDetails ml-10 md:ml-20">
             worry no more because we are here to <br /> help you with luggage
             handling and <br /> vehicle parking
@@ -17,17 +20,25 @@ const TopSection = () => {
           <img className="w-full" src={topSectionImg} alt="oqpai" />
         </div>
       </div>
-      <div className="flex flex-col-reverse  md:flex-row mt-20">
+
+      {/* bottom Section */}
+      <div className="flex flex-col-reverse  md:flex-row my-20">
         <div className="flex-1">
           <img src={bottomSectionImg} alt="" />
         </div>
         <div className="flex-1">
-            <h1 className="sectionTitle text-right mr-10 md:mr-20">Hassle free parkings all over the city</h1>
-            <p className="sectionDetails text-right mr-10 md:mr-20">Whether it's your car, your scooter or cycle <br/> we have space fo everyone and everything <br/>so that you can be tension free while your visit</p>
+          <h1 className="sectionTitle text-right mr-10 md:mr-20">
+            Hassle free parkings all over the city
+          </h1>
+          <p className="sectionDetails text-right mr-10 md:mr-20">
+            Whether it's your car, your scooter or cycle <br /> we have space fo
+            everyone and everything <br />
+            so that you can be tension free while your visit
+          </p>
         </div>
       </div>
     </main>
   );
 };
 
-export default TopSection;
+export default ServiceSection;
